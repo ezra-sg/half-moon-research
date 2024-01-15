5471ee48debe06f54937e25aab6cbc11b7092dda
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-15 13:29:44 -0500

    add vercel analytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+
     import '../app.scss';
     import SectionOne from '$lib/components/section-1.svelte';
+
+    // inject Vercel analytics
+    inject({ mode: dev ? 'development' : 'production' });
+    injectSpeedInsights();
 </script>
 
 <SectionOne />
