21218dad6ccebf713433ab125bf349e7ec6df634
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-16 10:55:25 -0500

    add rudimentary logo animation

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,4 +1,38 @@
+<script lang="ts">
+    import logo from '$lib/assets/logo--horizontal.svg';
+	import { onDestroy, onMount } from 'svelte';
+
+    let logoClass = 'c-nav__logo c-nav__logo--invisible';
+
+    function scrollHandler() {
+        const scrollAmount = window?.scrollY ?? 0;
+
+        if (scrollAmount > 200) {
+            logoClass = 'c-nav__logo';
+        } else {
+            logoClass = 'c-nav__logo c-nav__logo--invisible';
+        }
+    }
+
+    onMount(() => {
+        if (typeof window === 'undefined') return;
+        window?.addEventListener('scroll', scrollHandler);
+    });
+
+    onDestroy(() => {
+        if (typeof window === 'undefined') return;
+        window?.removeEventListener('scroll', scrollHandler);
+    });
+</script>
+
 <div class="c-nav">
+    <img
+        src="{logo}"
+        alt="Half-Moon Research Logo - Horizontal"
+        width="240"
+        class="{logoClass}"
+        aria-hidden="{logoClass === 'c-nav__logo'}"
+    />
     <div class="c-nav__hamburger-container">
         <div class="c-nav__hamburger-line"></div>
         <div class="c-nav__hamburger-line"></div>
@@ -8,11 +42,25 @@
 
 <style lang="scss">
 .c-nav {
+    position: sticky;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    top: 0;
+    padding: 8px;
+    background: white;
+
+    &__logo {
+        opacity: 1;
+        transition: opacity 0.3s ease;
+
+        &--invisible {
+            opacity: 0;
+        }
+    }
+
     &__hamburger-container {
         padding: 14px;
-        position: absolute;
-        top: 8px;
-        right: 8px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
