bfe189b5074429e08d0d4dbb8db11b664b5e5e8a
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-06-09 16:31:27 -0400

    missed new files

---
+++ src/lib/components/info-card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let text: string;
+    export let image: string;
+    export let imageSize: number;
+</script>
+
+<li class="border-l-[1px] border-glass mb-8 pl-4 flex gap-4 items-center">
+    <img src={image} alt="placeholder" height={imageSize} width={imageSize}>
+
+    { text }
+</li>
